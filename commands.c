ddecbcab935a3f68277f309e44a4a628da7befbb
Herwiggg <michelot.alexy78@gmail.com>
2024-01-13 17:12:17 +0100

    added few commands

--- commands.c
+++ commands.c
@@ -6,7 +6,7 @@
 /*   By: almichel <almichel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 00:52:25 by almichel          #+#    #+#             */
-/*   Updated: 2024/01/13 03:04:51 by almichel         ###   ########.fr       */
+/*   Updated: 2024/01/13 17:10:09 by almichel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,54 @@ void	ft_sa_sb(t_list **list)
 
 void	ft_ss(t_list **a, t_list **b)
 {
+	ft_sa_sb(a);
+	ft_sa_sb(b);
 }
 
-void	ft_pa_pb(t_list **list)
+void	ft_pa(t_list **a, t_list **b)
 {
+	t_list *head_a;
+	t_list *head_b;
+
+	if (!b || !*b)
+		return;
+	head_a = *a;
+	head_b = *b;
+	*b = (*b)->next;
+	(*b)->prev = NULL;
+	if (*a)
+		(*a)->prev = head_b;
+	head_b->next = head_a;
+	head_b->prev = NULL;
+	*a = head_b;
+}
+
+void	ft_pb(t_list **a, t_list **b)
+{
+	t_list *head_a;
+	t_list *head_b;
+
+	if (!a || !*a)
+		return;
+	head_a = *a;
+	head_b = *b;
+	*a = (*a)->next;
+	(*a)->prev = NULL;
+	if (*b)
+		(*b)->prev = head_a;
+	head_a->next = head_b;
+	head_a->prev = NULL;
+	*b = head_a;
 }
 
 void	ft_ra_rb(t_list **list)
 {
+	t_list *head;
+
+	head = *list;
+	*list = head->next;
+	head->next->prev = NULL;
+	head->next = NULL; 
+	ft_lstadd_back(list, head);
 }
 
-void	ft_rr(t_list **a, t_list **b)
-{
-}
--- commands2.c
+++ commands2.c
@@ -6,11 +6,16 @@
 /*   By: almichel <almichel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 01:04:23 by almichel          #+#    #+#             */
-/*   Updated: 2024/01/12 01:05:20 by almichel         ###   ########.fr       */
+/*   Updated: 2024/01/13 16:39:43 by almichel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+
+void	ft_rr(t_list **a, t_list **b)
+{
+}
 
 void	ft_rra_rrb(t_list **list)
 {
